33923e0a8c6c9b5ce12080313d59fb2436c7a4b4
Llyontt <52559058+Llyontt@users.noreply.github.com>
2019-07-11 15:29:47 +0800

    Add files via upload

--- Model/EnemyList.cpp
+++ Model/EnemyList.cpp
@@ -1,5 +1,17 @@
 #include "EnemyList.h"
 
+void EnemyList::AddEnemy(int atk, int def, int hp, int exp) {
+	Enemy enemy(hp, atk, def, exp);
+	m_enemies.push_back(enemy);
+}
+
+void EnemyList::ChangeEnemy(int atk, int def, int hp, int exp, int index) {
+	m_enemies[index].setAtk(atk);
+	m_enemies[index].setDef(def);
+	m_enemies[index].setHp(hp);
+	m_enemies[index].setExp(exp);
+}
+
 void EnemyList::SaveEnemyList(string name) {
 	string filename = "Game\\"+name;
 	if ((_access(filename.c_str(), 0) == -1))
@@ -10,11 +22,22 @@ void EnemyList::SaveEnemyList(string name) {
 	ofstream fout;
 	fout.open(path.c_str(), ios::out | ios::binary);
 	for (int i = 0; i < m_enemies.size(); i++) {
-		Enemy x = m_enemies[i];
-		fout.write(path.c_str(), sizeof(x.getHp()));
-		fout.write(path.c_str(), sizeof(x.getAtk()));
-		fout.write(path.c_str(), sizeof(x.getDef()));
-		fout.write(path.c_str(), sizeof(x.getExp()));
+		fout.write((char*)& m_enemies[i], sizeof(m_enemies[i]));
 	}
 	fout.close();
+}
+
+void EnemyList::LoadEnemyList(string name) {
+	string filename = "Game\\" + name;
+	if ((_access(filename.c_str(), 0) == -1))
+		if (_mkdir(filename.c_str()) != 0) {
+			cerr << "Directory is not exists and can't create the directory:" << filename << "\n";
+		}
+	string path = filename + "\\EnemyList.txt";
+	ifstream fcin;
+	fcin.open(path.c_str(), ios::out | ios::binary);
+	for (int i = 0; i < m_enemies.size(); i++) {
+		fcin.read ((char*)&m_enemies[i], sizeof(m_enemies[i]));
+	}
+	fcin.close();
 }
